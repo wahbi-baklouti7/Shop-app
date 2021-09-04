cc68c92f197eaae76aabce8d06bf8549637fcc38
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-09-04 18:56:10 +0100

    :sparkles: add home layout

--- lib/layouts/home_layout_screen.dart
+++ lib/layouts/home_layout_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/cubit/shopapp/shop_cubit.dart';
 
 class HomeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Matjar"),
-      ),
+    return BlocConsumer<ShopCubit, ShopState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 0.9,
+            title: Text("Matjar"),
+            actions: [IconButton(onPressed: (){}, icon: Icon(Icons.search))],
+          ),
+          body: ShopCubit.get(context)
+              .bottomScreen[ShopCubit.get(context).current_index],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: ShopCubit.get(context).current_index,
+            onTap: (index) {
+              ShopCubit.get(context).changeBottomNav(index);
+            },
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.apps), label: "Categories"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite), label: "Favorites"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: "Settings"),
+            ],
+          ),
+        );
+      },
     );
   }
 }
